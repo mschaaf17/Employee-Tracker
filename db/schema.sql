70878e8e21b54ed2c2d49a26c7bc8602712e4d0b
Maddy <madisonschaaf48@gmail.com>
2022-04-05 17:36:36 -0600

    added tables to schmea.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,29 @@
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+-- CREATE TABLE employee (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   industry_connected BOOLEAN NOT NULL
+-- );
+
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+
+)
+}
